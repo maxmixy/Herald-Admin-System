ab7e6798f1eda408534ca0c0e55491b0b43d0119
maxmixy <163897493+maxmixy@users.noreply.github.com>
2024-05-11 20:41:28 +0800

    Add files via upload

--- Login.php
+++ Login.php
@@ -7,23 +7,23 @@
     <title>Login Page</title>
     <link rel="stylesheet" href="AdminStyle.css">
 </head>
-<body>
-  <div class="bigBox">
-        
-        <div class="loginMain">
-            <div class="loginLogo">
-                <img src="TBHLogo.png" alt="Logo" style="width: 90%; height: 90%;">
-            </div>
-            <div class="loginContainer">
-                <form action="LogProc.php" method="post">
-                    <img src="The Bedan Herald.png" style="width: 100%; height: auto;">
-                    <input type="text" id="username" name="username" placeholder="Enter Username" required> <br>
-                    <input type="password" id="password" name="password" placeholder="Enter Password" required> <br>
-                    <button type="submit">     Login     </button>
-                </form>
-            </div>
+<body> 
+    <br><br><br>
+    <div class="loginMain">
+        <div class="loginLogo">
+            <img src="TBHLogo.png" alt="Logo" style="width: 70%; height: 70%;">
         </div>
-    </div>
+        <div class="loginContainer">
+            <form action="LogProc.php" method="post">
+                <br><br><br><br><br><br><br><br>
+                <img src="The Bedan Herald.png" style="width: 100%; height: auto;"><br><br><br><br><br>
+                <input type="text" id="username" name="username" placeholder="Enter Username" required> <br><br>
+                <input type="password" id="password" name="password" placeholder="Enter Password" required> <br><br>
+                <button type="submit">     Login     </button>
+                <br><br><br><br><br><br><br><br>
+            </form>
+        </div>
+    </div> 
 </body>
 </html> 
 
