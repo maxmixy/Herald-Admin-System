b72a1d995ab4279f528d01b36fa88ae132a702e8
maxmixy <163897493+maxmixy@users.noreply.github.com>
2024-05-12 22:43:29 +0800

    Add files via upload

--- Home.php
+++ Home.php
@@ -1,17 +1,47 @@
 <?php
 session_start();
-if (isset($_SESSION["MemberID"]) && isset($_SESSION["name"])) {
+include "db_conn.php";
+
+if (isset($_SESSION["MemberID"]) && isset($_SESSION["name"])) { 
     ?>
 
     <!DOCTYPE html>
     <html>
     <head>  
         <title>HOME</title>
+        <link rel="stylesheet" href="AdminStyle.css">
     </head>
 
-    <body>
-        <h1>Welcome, <?php echo $_SESSION['name'] ?></h1>
+    <div class="header">
+        <img src="The Bedan Herald.png" style="width: 30%; height: auto;" href="Home.php"> 
+        <a class="active" href="Home.php">Assignments</a>
+        <a href="#contact">Assign Tasks</a>
+        <a href="#about">Prorgess Overview</a>
         <a href="LogOut.php">Logout</a>
+    </div>
+
+    <body>
+        <br><br>
+        <h1>Welcome, <?php echo $_SESSION['name'] ?></h1>
+        <?php
+            $sql = "SELECT * FROM ArticleAssignment WHERE WriterID='$_SESSION["MemberID"]'";
+            $result = mysqli_query($conn, $sql);
+        
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $section = $row["Section"];
+                  $topic = $row["ArticleTopic"];  
+                  $notes = $row["Notes"];  
+                  $deadline = $row["Deadline"];  
+                  $status = $row["ArticlePreStatus"];  
+                  
+                  echo $section . $topic . $notes . $deadline . $status . "<br>";
+                }
+              } else {
+                echo "No assignments";
+              }
+            $conn->close();
+        ?>
     </body>
     </html>
     
