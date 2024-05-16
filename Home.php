126aae984aff6b3f3240e3c127b7792bb72b186b
myomaxi <mateomaxeneysabel@gmail.com>
2024-05-16 19:32:33 +0800

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,18 +1,60 @@
 <?php
 session_start();
-if (isset($_SESSION["MemberID"]) && isset($_SESSION["name"])) {
+include "db_conn.php";
+
+if (isset($_SESSION["MemberID"]) && isset($_SESSION["name"])) { 
     ?>
 
     <!DOCTYPE html>
     <html>
-    <head>  
-        <title>HOME</title>
-    </head>
+        <head>  
+            <title>HOME</title>
+            <link rel="stylesheet" href="AdminStyle.css">
+        </head>
 
-    <body>
-        <h1>Welcome, <?php echo $_SESSION['name'] ?></h1>
-        <a href="LogOut.php">Logout</a>
-    </body>
+        <body>
+            <div class="large-box" id="content">
+                <div class="header">
+                    <a href="Home.php"><img src="The Bedan Herald.png" style="width: 30%; height: auto;"> </a>
+                    <?php 
+                    if ($_SESSION["position"] == 'Section Editor' || $_SESSION["position"] == 'Head Admin' || $_SESSION["position"] == 'Admin'){
+                    ?>
+                   <tr> // buttons
+                    <td><a class="active" href="Home.php">Assignments</a></td>
+                    <td><a href="FeaturesTasks.php">Assign Tasks</a></td>
+                    <td><a href="#about">Progress Overview</a></td>
+                  </tr>
+                    <?php
+                    }
+                    ?>
+                    <a href="LogOut.php">Logout</a> 
+                </div> 
+                <br><br>
+                <h1>Good day <?php echo $_SESSION['name'] ?>, here are your active assignments: </h1>
+                <?php
+                    $sql = "SELECT * FROM ArticleAssignment WHERE WriterID='{$_SESSION['MemberID']}'";
+                    $result = mysqli_query($conn, $sql);
+                
+                    if ($result) {
+                        echo '<table>';
+                        echo '<tr><th>Section</th><th>Article Topic</th><th>Notes</th><th>Deadline</th><th>Status</th></tr>';
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $section = $row["Section"];
+                            $topic = $row["ArticleTopic"];  
+                            $notes = $row["Notes"];  
+                            $deadline = $row["Deadline"];  
+                            $status = $row["ArticlePreStatus"];  
+                            
+                            echo "<tr><td>$section</td><td>$topic</td><td>$notes</td><td>$deadline</td><td>$status</td></tr>";
+                        }
+                        echo '</table>';
+                    } else {
+                        echo "No assignments";
+                    }
+                    $conn->close();
+                ?>
+            </div> //Closing the large-box div 
+        </body>
     </html>
     
     <?php
